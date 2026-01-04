23d308188a275112dcb0cd4dba5244652400ecf2
duke <wwhbygx@sina.com>
2026-01-04 20:30:49 +0800

    fix(core): 修复ViewModel事件接收问题并优化组件生命周期管理
    - 修复ViewModel未收到事件的问题
    - 简化组件生命周期事件处理逻辑，移除复杂的手动生命周期注入
    - 更新@duke/component-lifecycle依赖从1.x到2.x版本
    - 更新@duke/view-model依赖从1.x到2.x版本
    - 优化自定义组件代码调用支持，不再独立维护事件缓存
    - 更新版本号到v2.2.1并在文档中同步更新版本标记

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # EventPost
 
 ## 简介
-[![openHarmony](https://img.shields.io/badge/openharmony-v2.1.2-brightgreen)](https://gitee.com/Duke_Bit/eventpost/releases/tag/v2.1.2)
+
+[![openHarmony](https://img.shields.io/badge/openharmony-v2.2.1-brightgreen)](https://gitee.com/Duke_Bit/eventpost/releases/tag/v2.2.1)
 
 EventPost是一款消息总线，具有生命周期感知能力，支持Sticky。
 
@@ -65,7 +66,18 @@ EventPost.getDefault().onFromComponent("msgId", this, (arg1:object, arg2:object)
 })
 ```
 
-- 三种注册方式任性其一，onFromComponent 和装饰器均能感知组件生命周期
+在ViewModel中使用onFromViewModel
+
+```typescript
+import { EventPost } from "eventpost";
+
+EventPost.getDefault().onFromViewModel("msgId", this, (arg1:object, arg2:object) => {
+})
+```
+ViewModel的使用请参考[ViewModel](https://gitee.com/Duke_Bit/view-model)
+直接new不加装饰器的话，还是会有内存泄露的
+
+- 四种注册方式任性其一，onFromComponent、onFromViewModel 和装饰器均能感知组件生命周期
 - 使用on的方法注册的话，需要自行off防止内存泄漏
 
 ### 发送消息
@@ -86,6 +98,7 @@ EventPost.getDefault().post("msgId", "arg1", { params1: "bbb" })
 | setAllSticky    | boolean                          | 配置基础消息发送是否为粘性事假，默认为粘性，跨线配置非粘性，则每个线程均要初始化 |
 | on              | string,function,sticky,callThis  | 注册订阅方法，并配置是否支持粘性                         |
 | onFromComponent | string,component,function,sticky | 注册订阅方法，并配置是否支持粘性，仅支持组件内注册，能够自动反注册        |           |
+| onFromViewModel | string,ViewModel,function,sticky | 注册订阅方法，并配置是否支持粘性，仅支持ViewModel内注册，能够自动反注册 |           |
 | off             | string,function                  | 反注册订阅方法                                  |
 | once            | string,function,callThis         | 注册一次性订阅方法                                |
 | post            | string,...args:any[]             | 发送消息（受配置影响）                              |
@@ -112,7 +125,6 @@ DevEco Studio: 5.0.5.315, SDK: HarmonyOS 5.0.1 Release Ohos_sdk_public 5.0.1.115
 |---- eventpost
 |     |---- AppScrope  # 示例代码文件夹
 |     |---- entry  # 示例代码文件夹
-|     |---- examples # 示例代码文件夹  
 |     |---- library # eventpost库文件夹  
 |           |---- build  # eventpost模块打包后的文件
 |           |---- src/main/ets/eventpost # EventPost主入口
@@ -132,10 +144,17 @@ DevEco Studio: 5.0.5.315, SDK: HarmonyOS 5.0.1 Release Ohos_sdk_public 5.0.1.115
 
 ## 其他库
 
-- [@duke/logan-ext](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Flogan-ext) Logan扩展库，方便开箱即用: [https://gitee.com/Duke_Bit/logan](https://gitee.com/Duke_Bit/logan)
-- [@duke/logan](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Flogan) Logan是一个为OpenHarmony开发的日志库，对美团技术团队的Logan的鸿蒙化移植版本: [https://gitee.com/Duke_Bit/logan](https://gitee.com/Duke_Bit/logan)
+- [@duke/view-model](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Fview-model)
+  鸿蒙版本的Lifecycle库，支持组件、Router和Navigation: [https://gitee.com/Duke_Bit/view-model](https://gitee.com/Duke_Bit/view-model)
+- [@duke/logan-ext](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Flogan-ext)
+  Logan扩展库，方便开箱即用: [https://gitee.com/Duke_Bit/logan](https://gitee.com/Duke_Bit/logan)
+- [@duke/logan](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Flogan)
+  Logan是一个为OpenHarmony开发的日志库，对美团技术团队的Logan的鸿蒙化移植版本: [https://gitee.com/Duke_Bit/logan](https://gitee.com/Duke_Bit/logan)
 - [@duke/websocket-client](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Fwebsocket-client)
   WebSocket库解决官方API的一些bug问题: [https://gitee.com/Duke_Bit/websocket](https://gitee.com/Duke_Bit/websocket)
-- [@duke/component-lifecycle](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Fcomponent-lifecycle) 鸿蒙版本的Lifecycle库，支持组件、Router和Navigation: [https://gitee.com/Duke_Bit/component-lifecycle](https://gitee.com/Duke_Bit/component-lifecycle)
-- [@duke/elf-dialog](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Felf-dialog) CustomDialog黑魔法 不依赖promptAction 实现的函数级弹窗，省去复杂的模版代码，让你专注于你的业务，一行代码搞定弹窗: [https://gitee.com/Duke_Bit/elf-dialog](https://gitee.com/Duke_Bit/elf-dialog)
-- [@duke/elf-refresh](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Felf-refresh) OpenHarmony 刷新组件，支持下拉刷新和上拉加载更多，支持各种组件，List、Grid，支持header，footer，目标打造HarmonyOS的SmartRefreshLayout: [https://gitee.com/Duke_Bit/ElfRefresh](https://gitee.com/Duke_Bit/ElfRefresh)
+- [@duke/component-lifecycle](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Fcomponent-lifecycle)
+  鸿蒙版本的Lifecycle库，支持组件、Router和Navigation: [https://gitee.com/Duke_Bit/component-lifecycle](https://gitee.com/Duke_Bit/component-lifecycle)
+- [@duke/elf-dialog](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Felf-dialog) CustomDialog黑魔法 不依赖promptAction
+  实现的函数级弹窗，省去复杂的模版代码，让你专注于你的业务，一行代码搞定弹窗: [https://gitee.com/Duke_Bit/elf-dialog](https://gitee.com/Duke_Bit/elf-dialog)
+- [@duke/elf-refresh](https://ohpm.openharmony.cn/#/cn/detail/@duke%2Felf-refresh) OpenHarmony
+  刷新组件，支持下拉刷新和上拉加载更多，支持各种组件，List、Grid，支持header，footer，目标打造HarmonyOS的SmartRefreshLayout: [https://gitee.com/Duke_Bit/ElfRefresh](https://gitee.com/Duke_Bit/ElfRefresh)
--- library/src/main/ets/EventPostExtensions.ts
+++ library/src/main/ets/EventPostExtensions.ts
@@ -1,118 +1,42 @@
 import { EventPost } from './EventPost'
 import { hilog } from '@kit.PerformanceAnalysisKit'
-import { Lifecycle, LifecycleOwner, LifecycleState, LIFECYCLE_DEFAULT, LIFECYCLE_INIT } from '@duke/component-lifecycle'
-import { EVENT_COMPONENT_EVENT_CACHE } from './Constants'
+import {
+  getCurrentLifecycle, LifecycleState } from '@duke/component-lifecycle'
 import { EventOnInfo } from './EventOnInfo'
-import { systemDateTime } from '@kit.BasicServicesKit'
-import { FrameNode, UIContext, uiObserver } from '@kit.ArkUI'
+import { ViewModel } from '@duke/view-model'
 
 EventPost.getDefault().onFromComponent =
   function (TypeName: string, component: any, callback: Function, sticky: boolean = false) {
-    if (!component.rerender) {
-      hilog.warn(0x0000, 'EventPost', '%{public}s', 'onFormComponent component is not a component')
+    let lifecycle = getCurrentLifecycle(component)
+    if (!lifecycle) {
+      hilog.warn(0x0000, 'EventPost', '%{public}s', 'onFormComponent component is not a lifecycle owner')
       return
     }
-    let methodName = `_${systemDateTime.getTime(true)}_${Math.random().toString(36).slice(-8)}`
-    component[methodName] = callback
-    if (component[EVENT_COMPONENT_EVENT_CACHE]) { //已经注入过生命周期了
-      let cache = component[EVENT_COMPONENT_EVENT_CACHE] as Array<EventOnInfo>
-      cache.push({
-        typeName: TypeName,
-        methodName: methodName,
-        sticky,
-        callBack: callback
-      })
-      let uiContext: UIContext = component.getUIContext()
-      let uniqueId: number = component.getUniqueId();
-      let frameNode: FrameNode = uiContext.getFrameNodeByUniqueId(uniqueId)
-      if (frameNode) { //aboutToAppear 已经执行完成，加缓存不会自动注册了，需要手动注册
-        this.dispatch.on(TypeName, callback, sticky, component)
-      }
-    } else {
-      this.dispatch.on(TypeName, callback, sticky, component)
-      let cache = new  Array<EventOnInfo>()
-      component[EVENT_COMPONENT_EVENT_CACHE] = cache
-      cache.push({
-        typeName: TypeName,
-        methodName: methodName,
-        sticky,
-        callBack: callback
-      })
-      if (component[LIFECYCLE_INIT]) { //有生命周期注入，但是没有缓存，则添加缓存
-        let keys = Object.keys(component)
-        for (let i = 0; i < keys.length; i++) {
-          let property = component[keys[i]]
-          if (property instanceof Lifecycle) {
-            property.addObserver((_, state) => {
-              if (state == LifecycleState.ON_DISAPPEAR) {
-                let cache = component[EVENT_COMPONENT_EVENT_CACHE] as Array<EventOnInfo>
-                cache.forEach(item => {
-                  EventPost.getDefault().off(item.typeName, component[item.methodName]);
-                })
-              }
-            })
-            break
-          }
+    EventPost.getDefault().on(TypeName, callback, sticky,component)
+    lifecycle.addObserver((_,event)=>{
+        if (event === LifecycleState.ON_DISAPPEAR) {
+          EventPost.getDefault().off(TypeName, callback)
         }
-      } else { //没有生命周期注入，则添加生命周期
-        let lifecycle = new Lifecycle()
-        lifecycle.addObserver((_, state) => {
-          if (state == LifecycleState.ON_DISAPPEAR) {
-            let cache = component[EVENT_COMPONENT_EVENT_CACHE] as Array<EventOnInfo>
-            cache.forEach(item => {
-              EventPost.getDefault().off(item.typeName, component[item.methodName]);
-            })
-          }
-        })
-        component[LIFECYCLE_DEFAULT] = lifecycle
-        let oldAppear = component.aboutToAppear
-        let oldDidBuild = component.onDidBuild
-        LifecycleOwner(component, LIFECYCLE_DEFAULT)
-        //已经错误时机，需要恢复，同时手动注入
-        component.aboutToAppear = oldAppear
-        component[LIFECYCLE_INIT].call(component)
-        let uiContext: UIContext = component.getUIContext()
-        let uniqueId: number = component.getUniqueId();
-        let frameNode: FrameNode = uiContext.getFrameNodeByUniqueId(uniqueId)
-        if (frameNode) { //获取到frameNode 已经错过注册事件，需要把didBuild还原
-          component.onDidBuild = oldDidBuild
-          component.lifecycleRegister.call(component)
-          //需要补充错过的生命周期
-          if (lifecycle.isPage) {
-            let pageInfo = uiContext.getPageInfoByUniqueId(uniqueId)
-            if (lifecycle.isRouterPage) {
-              let pageState = pageInfo.routerPageInfo.state
-              if (pageState == uiObserver.RouterPageState.ON_PAGE_SHOW ||
-                pageState == uiObserver.RouterPageState.ON_BACK_PRESS) {
-                lifecycle['handler'](LifecycleState.ON_SHOWED)
-              } else if (pageState == uiObserver.RouterPageState.ON_PAGE_HIDE) {
-                lifecycle['handler'](LifecycleState.ON_DISAPPEAR)
-              }
-            } else if (lifecycle.isNavigation) {
-              let pageState = pageInfo.navDestinationInfo.state
-              if (pageState == uiObserver.NavDestinationState.ON_SHOWN ||
-                pageState == uiObserver.NavDestinationState.ON_BACKPRESS) {
-                lifecycle['handler'](LifecycleState.ON_SHOWED)
-              } else if (pageState == uiObserver.NavDestinationState.ON_HIDDEN) {
-                lifecycle['handler'](LifecycleState.ON_DISAPPEAR)
-              } else if (pageState == uiObserver.NavDestinationState.ON_WILL_SHOW) {
-                lifecycle['handler'](LifecycleState.ON_WILL_SHOW)
-              } else if (pageState == uiObserver.NavDestinationState.ON_WILL_HIDE) {
-                lifecycle['handler'](LifecycleState.ON_WILL_HIDE)
-              } else if (pageState == uiObserver.NavDestinationState.ON_ACTIVE) {
-                lifecycle['handler'](LifecycleState.ON_ACTIVE)
-              } else if (pageState == uiObserver.NavDestinationState.ON_INACTIVE) {
-                lifecycle['handler'](LifecycleState.ON_INACTIVE)
-              }
-            }
-          } else {
-            if (frameNode.isVisible()) {
-              lifecycle['handler'](LifecycleState.ON_SHOWED)
-            } else {
-              lifecycle['handler'](LifecycleState.ON_HIDDEN)
-            }
-          }
-        }
-      }
+    })
+  }
+
+EventPost.getDefault().onFromViewModel = function (TypeName: string, viewModel: ViewModel, callback: Function, sticky: boolean = false) {
+  let cache:Array<EventOnInfo> | undefined = viewModel['_registerEventPost']
+  if(!cache){
+    cache = viewModel['_registerEventPost'] = new Array<EventOnInfo>()
+    let oldFunction = viewModel.clean
+    viewModel.clean = function () {
+      cache?.forEach(item => {
+        EventPost.getDefault().off(item.typeName, item.callBack)
+      })
+      oldFunction.call(ViewModel)
     }
   }
+  cache.push({
+    typeName: TypeName,
+    methodName: '',
+    sticky,
+    callBack: callback
+  })
+  EventPost.getDefault().on(TypeName, callback, sticky,viewModel)
+}
